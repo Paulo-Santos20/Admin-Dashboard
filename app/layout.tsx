674ed1acc90591a0dfdfb860ad02213da8e99f8d
Paulo-Santos20 <paulo_santos20@outlook.com.br>
2023-09-28 16:59:13 -0300

    style: pink

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="pt-br">
         <head>
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="favicon" href="/favicon.ico" />
         </head>
         <body className={inter.className}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
