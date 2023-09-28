e5d2e3cd2b9ab3c22c5614e6d6810f78114be81e
Paulo-Santos20 <paulo_santos20@outlook.com.br>
2023-09-28 12:22:30 -0300

    feat: stripe

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/providers/modal-provider";
 import { ToastProvider } from "@/providers/toast-provider";
+import { ThemeProvider } from "@/providers/theme-prodivers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,9 +21,11 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="pt-br">
         <body className={inter.className}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem >
           <ToastProvider />
           <ModalProvider />
           {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
